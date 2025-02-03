a354e808f067fde280b5d33a5046cc59fa6b9b6a
Lehlonolo Mokoena <126181969+lehlonolo27@users.noreply.github.com>
2025-02-03 22:34:11 +0200

    Update App.js for better readabilitY

--- PRJ371-Main-main/ARWSS/src/frontend/src/App.js
+++ PRJ371-Main-main/ARWSS/src/frontend/src/App.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Menu from './components/Menu';
-/*import Dashboard from './components/Dashboard';*/
 import Plastic from './components/Plastic';
 import Metal from './components/Metal';
 import Paper from './components/Paper';
 import Welcome from './components/Welcome';
 
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/Welcome" />} />
-        <Route path="/welcome" element={<Welcome />} /> 
-        <Route path="/login" element={<Login />} />
-        <Route path="/menu" element={<Menu />} />
-      
-        <Route path="/plastic" element={<Plastic />} />
-        <Route path="/metal" element={<Metal />} />
-        <Route path="/paper" element={<Paper />} />
-      </Routes>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Navigate replace to="/welcome" />} />
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/plastic" element={<Plastic />} />
+      <Route path="/metal" element={<Metal />} />
+      <Route path="/paper" element={<Paper />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
